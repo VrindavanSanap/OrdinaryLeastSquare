e58f7437fe7f985fb57dc57638de36401e4a65a9
vrindavan <vrindavansanap@gmail.com>
2023-01-10 15:11:58 +0530

    initial commit

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var data;
+
+function setup() {
+    createCanvas(500, 500);
+    background(0)     //Black
+}
+
+function mousePressed(){
+    console.log("Mouse Pressed")
+}
+// lol comment
+function draw(){
+
+}
