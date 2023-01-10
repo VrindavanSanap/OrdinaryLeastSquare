34967f858870a386c0476567f0d5474e01880fd9
vrindavan <vrindavansanap@gmail.com>
2023-01-10 15:23:55 +0530

    Fixed variable scopes

--- sketch.js
+++ sketch.js
@@ -7,15 +7,15 @@ function setup() {
 
 function mousePressed(){
     console.log("Mouse Pressed")
-    var x = map(mouseX, 0, width, 0, 1)
-    var y = map(mouseY, 0, height, 1, 0)
-    var point = createVector(x, y)
+    let x = map(mouseX, 0, width, 0, 1)
+    let y = map(mouseY, 0, height, 1, 0)
+    let point = createVector(x, y)
     data.push(point)
 }
-// lol comment
+
 function draw(){
     background(0)
-    for (var i = 0; i < data.length; i++){
+    for (let i = 0; i < data.length; i++){
         var x = map(data[i].x, 0, 1, 0, width)
         var y = map(data[i].y, 0, 1, height, 0)
 
